634c92539fb6aeed87116687f842a9dc9740d9df
“Ailuntz” <ailuntz@icloud.com>
2024-09-22 04:04:44 +0000

    Update GitHub Actions workflow with Tauri configuration

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,9 @@
+mod setup;
+
+fn main() {
+    let context = tauri::generate_context!();
+    tauri::Builder::default()
+        .setup(setup::init)
+        .run(context)
+        .expect("error while running OhMyBox application");
+}
